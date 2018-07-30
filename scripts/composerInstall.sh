7b6b63588eacd3c25949c12bc2b6ee30b2443548
DESKTOP-LVTGD6T\daumi <dauminhquantlu@gmail.com>
2018-07-30 12:56:31 +0700

    fijghsj

---
+++ scripts/composerInstall.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd /var/www/http/public | composer install > /var/log/install.out 2>&1
--- scripts/installapache.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-yum install -y httpd24 php72 mysql56-server php72-mysqlnd> /var/log/installapache.out 2>&1
