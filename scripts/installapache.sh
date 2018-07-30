f9e4b4a4d11b1680024ff53b68145c1c3f2ced2d
DESKTOP-LVTGD6T\daumi <dauminhquantlu@gmail.com>
2018-07-30 11:35:01 +0700

    fij

--- scripts/installapache.sh
+++ scripts/installapache.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # yum -y install httpd > /var/log/installapache.out 2>&1
-yum update -y |
-yum install -y httpd24 php72 mysql56-server php72-mysqlnd > /var/log/installapache.out 2>&1
+yum install -y httpd24 php70 mysql56-server php70-mysqlnd> /var/log/installapache.out 2>&1
