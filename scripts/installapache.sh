a9ecc3bd679ddd4d3bc88ce98d4a4d8104b2c8dd
DESKTOP-LVTGD6T\daumi <dauminhquantlu@gmail.com>
2018-07-30 11:25:43 +0700

    dsadsa

--- scripts/installapache.sh
+++ scripts/installapache.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-yum -y install httpd > /var/log/installapache.out 2>&1
+# yum -y install httpd > /var/log/installapache.out 2>&1
+yum install -y httpd24 php72 mysql56-server php72-mysqlnd > /var/log/installapache.out 2>&1
