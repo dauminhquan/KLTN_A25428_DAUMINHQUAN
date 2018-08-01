96c235004b66b364d55c665fd957022498b8152a
DESKTOP-LVTGD6T\daumi <dauminhquantlu@gmail.com>
2018-08-01 20:01:45 +0700

    fix comand

--- scripts/removeProject.sh
+++ scripts/removeProject.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo rm -rf /var/www/html/kltn && sudo mkdir -rf /var/www/html/kltn && sudo chmod 775 -R /var/www/html/kltn && sudo chmod 777 -R /var/www/html/kltn/storage
+sudo rm -rf /var/www/html/kltn && sudo mkdir /var/www/html/kltn && sudo chmod 775 -R /var/www/html/kltn && sudo chmod 777 -R /var/www/html/kltn/storage
