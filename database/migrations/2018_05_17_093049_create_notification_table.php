214403ac9f92a97af361d699362b80924d60c74d
QUANDAUCOMPUTER\quand <dauminhquantlu@gmail.com>
2018-08-07 16:28:36 +0700

    f

--- database/migrations/2018_05_17_093049_create_notification_table.php
+++ database/migrations/2018_05_17_093049_create_notification_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateNotificationTable extends Migration
+class CreateNotifiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateNotificationTable extends Migration
      */
     public function up()
     {
-        Schema::create('notifications', function (Blueprint $table) {
+        Schema::create('notifies', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('admin_id');
             $table->string('title');
@@ -30,6 +30,6 @@ class CreateNotificationTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('notifications');
+        Schema::dropIfExists('notifies');
     }
 }
